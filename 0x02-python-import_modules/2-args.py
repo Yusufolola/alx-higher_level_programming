c2b5f46f069b3b976c56708463ebffee6ef816fd
yusufolola <yusufoyesanmi@yahoo.com>
2024-01-05 11:42:55 -0800

    success

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,7 +9,7 @@ if __name__=="__main__":
             print(f"{len(sys.argv) - 1}: {sys.argv[1]}")
 
         else:
-             print(f"{len(sys.argv) - 1} argument")
+             print(f"{len(sys.argv) - 1} arguments")
              for i in range(1, len(sys.argv)):
                  print(f"{i}: {sys.argv[i]}")
 
