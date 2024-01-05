4addaa3b46633507dd414ddcfcfbc4913dbe68d2
yusufolola <yusufoyesanmi@yahoo.com>
2024-01-05 11:35:47 -0800

    success

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+if __name__=="__main__":
+    import sys
+    if (len(sys.argv) == 1):
+            print("0 arguments .")
+    elif (len(sys.argv) > 1):
+        if (len(sys.argv) == 2):
+            print(f"{len(sys.argv) - 1} argument:")
+            print(f"{len(sys.argv) - 1}: {sys.argv[1]}")
+
+        else:
+             print(f"{len(sys.argv) - 1} argument")
+             for i in range(1, len(sys.argv)):
+                 print(f"{i}: {sys.argv[i]}")
+
+            
+
+
---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__=="__main__":
+    import sys
+    argv = len(sys.argv)
+    add = 0
+    if ((argv) == 1):
+        print("0")
+    elif ((argv) > 1):
+        for i in range (1, argv):
+            add += int(sys.argv[i])
+        print(add)    
+
