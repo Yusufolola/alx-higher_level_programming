a8bc2b070621b2f2d38bf409268fe076f7f1709a
yusufolola <yusufoyesanmi@yahoo.com>
2024-01-05 11:59:45 -0800

    lets go

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,17 +2,18 @@
 if __name__=="__main__":
     import sys
 
-if (len(sys.argv) == 1):
-    print("0 arguments .")
-elif (len(sys.argv) > 1):
-     if (len(sys.argv) == 2):
-         print(f"{len(sys.argv) - 1} argument:")
-         print(f"{len(sys.argv) - 1}: {sys.argv[1]}")
+argv = sys.argv
+if (len(argv) == 1):
+    print(f"{len(argv) - 1} arguments .")
+elif (len(argv) > 1):
+     if (len(argv) == 2):
+         print(f"{len(argv) - 1} argument:")
+         print(f"{len(argv) - 1}: {argv[1]}")
 
      else:
-         print(f"{len(sys.argv) - 1} arguments")
-         for i in range(1, len(sys.argv)):
-            print(f"{i}: {sys.argv[i]}")
+         print(f"{len(argv) - 1} arguments")
+         for i in range(1, len(argv)):
+            print(f"{i}: {argv[i]}")
 
             
 
