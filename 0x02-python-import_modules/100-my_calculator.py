1e24dfcaaa052ff2a0729efc4c6a2272e482c075
yusufolola <yusufoyesanmi@yahoo.com>
2024-01-05 19:45:34 -0800

    great

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,12 +2,14 @@
 if __name__=="__main__":
     import sys
     from calculator_1 import add, sub, mul, div
-    a = int(sys.argv[1])
-    operator = (sys.argv[2])
-    b = int(sys.argv[3])
-    if (len(sys.argv)) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+
+a = int(sys.argv[1])
+operator = (sys.argv[2])
+b = int(sys.argv[3])
+if (len(sys.argv)) != 4:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    sys.exit(1)
+elif (len(sys.argv)) == 4:
     if operator == "+":
         print(f"{a} {operator} {b} = {add(a, b)}")
     elif operator == "-":
@@ -18,4 +20,4 @@ if __name__=="__main__":
          print(f"{a} {operator} {b} = {div(a, b)}")
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit (1)
+        sys.exit(1)
