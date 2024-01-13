48782ab36990ee6d12c2f35c0f9dc2c188e3fb3f
yusufolola <yusufoyesanmi@yahoo.com>
2024-01-13 12:07:08 -0800

    cool

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if (number < 0):
+        last = abs(number) % 10
+        print(f"{last}", end="")
+    else:
+        last = (number) % 10
+        print(f"{last}", end="")
+    return (last)
