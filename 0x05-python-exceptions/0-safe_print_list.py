41547dfcc7a40d9693b4eda3d5ef4d6b0a58abf6
yusufolola <yusufoyesanmi@yahoo.com>
2024-01-24 21:28:02 +0100

    hi

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print("{}".format(my_list[i]), end="")
             idx = idx + 1
-    except (TypeError, IndexError, ValueError):
-        print("")
-        return idx
+    except (TypeError, NameError , IndexError, ValueError):
+        pass
+    print("")
+    return (idx)
