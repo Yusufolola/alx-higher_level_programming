1b2aebbd73fc5436ef7017c518fb8fbabcd2eb85
yusufolola <yusufoyesanmi@yahoo.com>
2024-03-05 06:53:23 +0100

    classes

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""class square with size"""
+
+
+class Square:
+    """size instantiation"""
+
+    def __init__(self , size):
+        """instance"""
+
+        self.__size = size  
+
