7a5599b54417f4395485e3ffa13cadd90adcf878
yusufolola <yusufoyesanmi@yahoo.com>
2024-03-05 07:32:25 +0100

    classes

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""class square with size"""
+
+
+class Square:
+    """size instantiation"""
+
+    def __init__(self , size = 0):
+        """instance"""
+
+        if not isinstance(size ,int):
+            raise TypeError(" size must be an integer")
+
+        if size < 0 :
+            raise ValueError("size must be >= 0")
+
+        self.__size = size  
+    
+    @getter
+    def size(self):
+        """get the size"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value ,int):
+            raise TypeError(" size must be an integer")
+
+        if (value < 0) :
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
+
+    def area(self):
+        """returns the current area"""
+
+        return (self.__size * self.__size)
+
