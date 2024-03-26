5b6f5d302827a354b3692f12bb1e65f6ab617a45
yusufolola <yusufoyesanmi@yahoo.com>
2024-03-26 22:35:10 +0100

    geometry

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""a function that returns true if the object is an instance of or if the 
-    the object is an instance of a class that inherited fromthe specified class otherwise false.
+"""a function that returns true if the object is an instance of or if the
+    the object is an instance of a class that inherited fromthe specified
+    class otherwise false.
 """
+
 
 def is_kind_of_class(obj, a_class):
     """prottype"""
 
-    if isinstance (obj, a_class):
+    if isinstance(obj, a_class):
         return True
     else:
         return False
