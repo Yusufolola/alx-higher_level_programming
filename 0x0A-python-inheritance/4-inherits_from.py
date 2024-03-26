2c724ad2a3e822588d2f2758553f83fdbab619de
yusufolola <yusufoyesanmi@yahoo.com>
2024-03-26 22:37:38 +0100

    geometry

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-"""a function that return true if the object is an instance of a class that inherited from the specified class.
+"""a function that return true if the object is an instance of
+a class that inherited from the specified class.
 """
+
 
 def inherits_from(obj, a_class):
     """prototype"""
 
-    if issubclass (type(obj), a_class):
+    if issubclass(type(obj), a_class):
         return True
     else:
         return False
