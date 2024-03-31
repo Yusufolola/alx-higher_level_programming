7dddaa80f6fc8177dc7edf038a396ec1398b95ee
yusufolola <yusufoyesanmi@yahoo.com>
2024-03-31 15:56:02 +0100

    up to date

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""function that reads a text file"""
+
+
+def read_file(filename=""):
+    """function that reads a text file"""
+    
+    with open("filename", "r") as f:
+        content = f.read()
+        print(content, end='')
