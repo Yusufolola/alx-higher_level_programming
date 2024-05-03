ce4f449c9947a00dde885b90b5e588046e5a5979
yusufolola <yusufoyesanmi@yahoo.com>
2024-05-03 07:57:05 +0100

    curl

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Display only body of a 200 status code response
-curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q 200 && curl -sl "$1"
+curl -s -o /dev/null -w "%{http_code}" "$1" | grep -q 200 && curl -sL "$1"
